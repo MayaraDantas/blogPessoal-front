1d7a9270da7f76f83e8a7c23886385f79f595e7e
MayaraDantas <mayarau.ugatinha25@gmail.com>
2021-06-03 18:51:44 -0300

    subindo get

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
-import { AuthService } from '../servece/auth.service';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
-import { AuthService } from '../servece/auth.service';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
